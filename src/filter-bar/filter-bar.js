02b2a332b22f42bf176cd8aa5479717f1c808e84
Barcari Dionis <62211382+ancara22@users.noreply.github.com>
2022-05-03 00:36:32 +0100

    v0.5

--- src/filter-bar/filter-bar.js
+++ src/filter-bar/filter-bar.js
@@ -1,12 +1,16 @@
 import React, {Component} from "react";
-import "./filter-bar.css"
+import "./filter-bar.css";
+
+
+
 
 export default class FilterBar extends Component {
 
     render() {
         return (
             <div className="filter-bar">
-                <span>+</span>
+                <div className="menuIcon"></div>
+        
             </div>
         )
     }
