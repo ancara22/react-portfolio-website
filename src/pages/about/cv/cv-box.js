98bc4adb69f55b3bb5895983196cbe0cab696e54
Barcari Dionis <62211382+ancara22@users.noreply.github.com>
2022-05-16 18:40:06 +0100

    I added my cv

--- src/pages/about/cv/cv-box.js
+++ src/pages/about/cv/cv-box.js
@@ -1,14 +1,44 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import "./cv-box.css";
+import { Document, Page, pdfjs } from "react-pdf";
+import Footer from "../../../footer";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const MyCv = ({classN}) => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+
+
   
   return (
 
     <div className={"my_cv " + classN} >
+      <Document className="cv-file" file="mycv.pdf" onLoadSuccess={onDocumentLoadSuccess} renderMode="svg">
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
 
+      <div className='buttons-box'>
 
+        <button 
+          onClick={()=>{
+            if(!(pageNumber-1 < 1)) setPageNumber(pageNumber-1)
+        }}>Back</button>
+
+         <button onClick={()=>{
+            if(!(pageNumber+1 > numPages)) setPageNumber(pageNumber+1)
+        }}>Next</button>
+      </div>
+      
     </div>
   );
 }
