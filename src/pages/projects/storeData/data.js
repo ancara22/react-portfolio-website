cfb9c95c73734b7e1213646c8c9f2ae68638c011
ancara22 <62211382+ancara22@users.noreply.github.com>
2022-05-24 21:39:33 +0100

    data adding

--- src/pages/projects/storeData/data.js
+++ src/pages/projects/storeData/data.js
@@ -3,49 +3,72 @@
 const dataBase = [
     { 
         title: "Portfolio Website",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration ", 
-        githublink: "https://www.lipsum.com",
-        netlifylink : "https://www.lipsum.com",
-        img : "img1.png",
+        description: "In Progress...", 
+        githublink: "https://github.com/ancara22/react-portfolio-website.git",
+        netlifylink : "In Progress...",
+        img : "react-portfolio-website.png",
         languages: ["React", "JavaScript", "CSS"],
         level: 1,
         id: 100
 
     },
     { 
-        title: "Pig Game",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.", 
-        githublink: "https://www.lipsum.com",
-        netlifylink : "https://www.lipsum.com",
-        img : "img1.png",
-        languages: ["React", "JavaScript", "CSS"],
+        title: "Dice Game",
+        description: "In Progress...", 
+        githublink: "https://github.com/ancara22/Dice-game.git",
+        netlifylink : "In Progress...",
+        img : "dice-game.png",
+        languages: ["JavaScript", "CSS"],
         level: 1,
         id: 101
        
     },
     { 
-        title: "Project 3",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look ", 
-        githublink: "https://www.lipsum.com",
-        netlifylink : "https://www.lipsum.com",
-        img : "img2.png",
-        languages: ["React", "JavaScript", "CSS"],
+        title: "Web Banking",
+        description: "In Progress...", 
+        githublink: "https://github.com/ancara22/Web-Banking-app.git",
+        netlifylink : "In Progress...",
+        img : "web-banking.png",
+        languages: ["JavaScript", "CSS"],
         level: 1,
         id: 102
        
     },
     { 
-        title: "Project 4",
-        description: "There are many variations of passages of Lorem Ipsum available, but the majority", 
-        githublink: "https://www.lipsum.com",
-        netlifylink : "https://www.lipsum.com",
-        img : "img1.png",
-        languages: ["React", "JavaScript", "CSS"],
+        title: "Puzzle Game",
+        description: "In Progress...", 
+        githublink: "https://github.com/ancara22/Puzzle-game.git",
+        netlifylink : "In Progress...",
+        img : "puzzle-game.png",
+        languages: ["JavaScript", "CSS"],
+        level: 1,
+        id: 103
+       
+    },
+    { 
+        title: "Ping Pong Game",
+        description: "In Progress...", 
+        githublink: "https://github.com/ancara22/Ping-Pong-Game.git",
+        netlifylink : "In Progress...",
+        img : "ping-pong-game.png",
+        languages: ["Racket"],
+        level: 1,
+        id: 103
+       
+    },
+    { 
+        title: "Euler Circuit Finder",
+        description: "In Progress...", 
+        githublink: "https://github.com/ancara22/Euler-Circuit-computing.git",
+        netlifylink : "In Progress...",
+        img : "euler-circuit-finder.png",
+        languages: ["JavaScript", "CSS"],
         level: 1,
         id: 103
        
     }
 ]
+
 
 
 export default dataBase;
